81543b8fbb5faf55a0a9946314299c5089ffee72
Srinivas <sairao1996@gmail.com>
2015-09-11 18:02:01 +0530

    Added gitignore and android code

---
+++ gpml/src/com/example/gpml/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.gpml;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity implements OnClickListener{
+
+	private Button startButton;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+	@Override
+	public void onClick(View v) {
+		if ( startButton.isPressed() )
+		{
+			String[] args = new String[10];
+			GaussianProcess.main(args);
+		}
+		
+	}
+}
